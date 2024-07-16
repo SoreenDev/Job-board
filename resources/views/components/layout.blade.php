e4c58a7e6d061a2b8199e5b442e7953062e6f5a9
soreendev <soreendev@gmail.com>
2024-07-16 21:49:05 +0430

    fix bug Tailwind

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+
+    <title>Laravel</title>
+
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <!-- Styles -->
+
+
+</head>
+<body class="mx-auto mt-10 max-w-2xl bg-gray-600 text-slate-700">
+    {{ $slot }}
+</body>
+
+</html>

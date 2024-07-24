7651ecc5b31d6bc229736e9049904c6fb3b298c8
soreendev <soreendev@gmail.com>
2024-07-24 23:07:34 +0430

    add filter ui p2 complete search part

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -2,24 +2,27 @@
     <x-breadcrumbs class="mb-4" :links="['Jobs'=> route('job.index')]" />
 
     <x-card class="mb-4 text-sm">
-        <div class="mb-4 grid grid-cols-2 gap-4">
+        <form action="{{route("job.index")}}" method="GET">
+            <div class="mb-4 grid grid-cols-2 gap-4">
 
-            <div>
-                <div class="mb-1 font-semibold">Search</div>
-                <x-text-input value="" name="search" placeholder="Search for any text" />
-            </div>
-
-            <div>
-                <div class="mb-1 font-semibold">Salary</div>
-                <div class="flex space-x-2  ">
-                    <x-text-input value="" name="min_salary" placeholder="From" />
-                    <x-text-input value="" name="max_salary" placeholder="To" />
+                <div>
+                    <div class="mb-1 font-semibold">Search</div>
+                    <x-text-input value="" name="search" placeholder="Search for any text" />
                 </div>
-            </div>
-            <div>3</div>
-            <div>4</div>
 
-        </div>
+                <div>
+                    <div class="mb-1 font-semibold">Salary</div>
+                    <div class="flex space-x-2  ">
+                        <x-text-input value="" name="min_salary" placeholder="From" />
+                        <x-text-input value="" name="max_salary" placeholder="To" />
+                    </div>
+                </div>
+                <div>3</div>
+                <div>4</div>
+
+                <button class="w-full">Filter</button>
+            </div>
+        </form>
     </x-card>
 
     @foreach($jobs as $job)

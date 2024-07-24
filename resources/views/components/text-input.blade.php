9b60a8c661e4066ed9474e2ce30acc71536152fe
soreendev <soreendev@gmail.com>
2024-07-24 22:03:08 +0430

    add filter ui p1

---
+++ resources/views/components/text-input.blade.php
@@ -0,0 +1,2 @@
+<input type="text" name="{{ $name }}" id="{{ $name }}" value="{{ $value }}" placeholder="{{ $placeholder }}"
+class="w-full rounded-md border-0 py-1.5 px-2.5 text-sm ring-1 ring-slate-500 placeholder:text-slate-400 focus:ring-2">

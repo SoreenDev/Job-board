1834ce31202354215f5b23fb1046fbc113df3902
MJ-h <soreendev@gmail.com>
2024-08-01 11:51:00 +0330

    add config employer model-migrations

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Job extends Model
 {
     use HasFactory;
     public static array $experience = ['junior','mid_level','senior'];
     public static array $category = ['Finance','sales','Marketing','it'];
+
+    public function employer() : BelongsTo
+    {
+        return $this->belongsTo(Employer::class);
+    }
 }
--- database/migrations/2024_07_14_161038_create_jobs_table.php
+++ database/migrations/2024_07_14_161038_create_jobs_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->string('location');
             $table->string('category');
             $table->enum('experience', Job::$experience);
+            $table->foreignIdFor(\App\Models\Employer::class);
             $table->timestamps();
         });
     }

f5572640ff2eda9d8a36742918aedef51b4f433d
MJ-h <soreendev@gmail.com>
2024-08-05 11:21:26 +0330

    add relations

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Job extends Model
 {
@@ -15,5 +16,10 @@ class Job extends Model
     public function employer() : BelongsTo
     {
         return $this->belongsTo(Employer::class);
+    }
+
+    public function jobapplication() : HasMany
+    {
+        return $this->hasMany(JobApplication::class);
     }
 }

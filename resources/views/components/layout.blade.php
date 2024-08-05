57538641ccdbf861ccd8e892e6cc7d30ca69dde7
MJ-h <soreendev@gmail.com>
2024-08-05 11:24:55 +0330

    cheng background ..

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,13 @@
     <title>Laravel</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="mx-auto mt-10 max-w-2xl bg-gray-600 text-slate-700">
+<style>
+    .body{
+        background: rgb(2,0,36);
+        background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(92,9,119,1) 37%, rgba(95,9,121,1) 57%, rgba(2,0,36,1) 100%);
+    }
+</style>
+<body class="body mx-auto mt-10 max-w-2xl bg-gray-600 text-slate-700">
 
 <nav class="mb-8 flex justify-between text-2xl font-medium text-neutral-50">
     <ul class=" flex space-x-2">

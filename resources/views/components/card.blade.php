c599b7368ad47a9cab762ed9065eaa2a9e8aa07a
MJ-h <soreendev@gmail.com>
2024-08-05 11:24:41 +0330

    cheng background ..

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,3 +1,3 @@
-<div {{ $attributes->merge(['class' => 'rounded-md border border-slate-300 bg-white p-4 shadow-sm']) }}>
+<div {{ $attributes->merge(['class' => 'rounded-md border border-slate-300 bg-white bg-opacity-90 p-4 shadow-sm']) }}>
 {{ $slot }}
 </div>

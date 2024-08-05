611db3dc4a63c9662a4c8f6eb7d2e5e7b3deef96
MJ-h <soreendev@gmail.com>
2024-08-05 11:54:35 +0330

    config JobApplicationController-store

---
+++ app/Http/Requests/jobApplication/StoreJobApplicationRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Requests\jobApplication;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreJobApplicationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'expected_salary' => ['required', 'numeric' , 'min:1' , 'max:1000000'],
+        ];
+    }
+}
